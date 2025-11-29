939dc02ca1ee7826ab48dec0c11738f307ea6f3d
MidnightGhostmare <juanpa20037@gmail.com>
2025-11-28 21:42:10 -0500

    Release: -Add edit user form -Add change user password

--- BasicAPI/Employees.Backend/Repositories/Implementations/UsersRepository.cs
+++ BasicAPI/Employees.Backend/Repositories/Implementations/UsersRepository.cs
@@ -18,14 +18,34 @@ public class UsersRepository : IUsersRepository
         DataContext context,
         UserManager<User> userManager,           //Nos provee los metodos para administrar usuarios
         RoleManager<IdentityRole> roleManager  //Nos provee los metodos para administrar roles
-        ,SignInManager<User> signInManager)
+        , SignInManager<User> signInManager)
     {
         _context = context;
         _userManager = userManager;
         _roleManager = roleManager;
         _signInManager = signInManager;
     }
-        
+
+    public async Task<User> GetUserAsync(Guid userId)
+    {
+        var user = await _context.Users
+            .Include(u => u.City!)
+            .ThenInclude(c => c.State!)
+            .ThenInclude(s => s.Country)
+            .FirstOrDefaultAsync(x => x.Id == userId.ToString());
+        return user!;
+    }
+
+    public async Task<IdentityResult> ChangePasswordAsync(User user, string currentPassword, string newPassword)
+    {
+        return await _userManager.ChangePasswordAsync(user, currentPassword, newPassword);
+    }
+
+    public async Task<IdentityResult> UpdateUserAsync(User user)
+    {
+        return await _userManager.UpdateAsync(user);
+    }
+
     public async Task<SignInResult> LoginAsync(LoginDTO model)
     {
         return await _signInManager.PasswordSignInAsync(model.Email, model.Password, false, false);
---
+++ BasicAPI/Employees.Shared/DTOs/ChangePasswordDTO.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Employees.Shared.DTOs;
+
+public class ChangePasswordDTO
+{
+    [DataType(DataType.Password)]
+    [Display(Name = "Current Password")]
+    [StringLength(20, MinimumLength = 6, ErrorMessage = "The field {0} must have between {2} and {1} characters.")]
+    [Required(ErrorMessage = "The field {0} is required.")]
+    public string CurrentPassword { get; set; } = null!;
+
+    [DataType(DataType.Password)]
+    [Display(Name = "New Password")]
+    [StringLength(20, MinimumLength = 6, ErrorMessage = "The field {0} must have between {2} and {1} characters.")]
+    [Required(ErrorMessage = "The field {0} is required.")]
+    public string NewPassword { get; set; } = null!;
+
+    [Compare("NewPassword", ErrorMessage = "The new password and confirmation do not match.")]
+    [DataType(DataType.Password)]
+    [Display(Name = "Confirm New Password")]
+    [StringLength(20, MinimumLength = 6, ErrorMessage = "The field {0} must have between {2} and {1} characters.")]
+    [Required(ErrorMessage = "The field {0} is required.")]
+    public string Confirm { get; set; } = null!;
+}
